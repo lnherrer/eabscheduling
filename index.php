f8c342ff762cd636fce4461c1064960d48ecdb7d
Omar Ramadan <ramoram3@gmail.com>
2014-03-20 15:42:27 -0500

    include:index part 1

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
-
     <title>Theme Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
