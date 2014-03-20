057c81830c93dda040e1376192197f39e78fe4e0
Omar Ramadan <ramoram3@gmail.com>
2014-03-20 16:45:40 -0500

    menu updates 2

--- includes/menu.php
+++ includes/menu.php
@@ -28,24 +28,20 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">EAB Scheduling: Home</a>
+          <a class="navbar-brand" href="index.php">EAB Scheduling</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li><a href="index.php#about">About</a></li>
-            <li><a href="index.php#register_now">Register Now</a></li>
-            <li><a href="index.php#sign_up_now">Sign Up Now</a></li>
             <li><a href="index.php#contact">Contact</a></li>
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <a href="index.php#sign_up_now" class="dropdown-toggle" data-toggle="dropdown">Sign Up Now <b class="caret"></b></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
+                <li><a href="index.php#im_new">I'm New</a></li>
+                <li><a href="index.php#been_there_done_that">Been There, Done That</a></li>
                 <li class="divider"></li>
                 <li class="dropdown-header">Nav header</li>
                 <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
               </ul>
             </li>
           </ul>
