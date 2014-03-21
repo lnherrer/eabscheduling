70233a666ab4afc6f0d39435a0effa8405a4e101
lnherrer <lnsherrer@gmail.com>
2014-03-21 12:29:55 -0500

    updates 1.5

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
         <h1>EAB Scheduling</h1>
         <p>Last test commit!</p>
-        <p><a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a></p> <p><a href="returning_volunteer.php" class="btn btn-primary btn-lg" role="button">Returning Volunteer &raquo;</a></p><br />
+        <p><a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a><a href="returning_volunteer.php" class="btn btn-primary btn-lg" role="button">Returning Volunteer &raquo;</a></p><br />
         <p><a href="calendar.php" class="btn btn-primary btn-lg" role="button">Calendar/Schedule &raquo;</a></p>
 
 <?php require_once("includes/footer.php"); ?>
