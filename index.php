7c0cdcdfe72c472f2d3fb4a93ee54e90c76b01cb
Omar Ramadan <ramoram3@gmail.com>
2014-03-21 15:52:10 -0500

    index text updates 3

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 
         <h1>EAB Scheduling</h1>
         <p>Last test commit!</p>
-        <p>Haven't volunteered with EAB (at EAB Clinic, M-Power, or a community screening) before? Sign up now!</p><br />
+        <p>Haven't volunteered with EAB (at EAB Clinic, M-Power, or a community screening) before? Sign up now!</p>
         <p><a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a></p><br /><br />
-        <p>Volunteered with EAB already but just can't get enough? Sign up again!</p><br />
+        <p>Volunteered with EAB already but just can't get enough? Sign up again!</p>
         <p><a href="returning_volunteer.php" class="btn btn-primary btn-lg" role="button">Returning Volunteer &raquo;</a></p><br /><br />
-        <p>Prefer calendar mode? You do you!</p><br />
+        <p>Prefer calendar mode? You do you!</p>
         <p><a href="calendar.php" class="btn btn-primary btn-lg" role="button">Calendar/Schedule &raquo;</a></p><br /><br />
 
 <?php require_once("includes/footer.php"); ?>
