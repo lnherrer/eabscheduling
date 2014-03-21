99b87f66a1361cb433e7c1be74401142356f7e2e
lnherrer <lnsherrer@gmail.com>
2014-03-21 14:59:41 -0500

    1.2.2

--- includes/menu.php
+++ includes/menu.php
@@ -63,37 +63,7 @@ if($new == "volunteer")
 		        <h4 class=\"modal-title\" id=\"New Volunteer\">New Volunteer</h4>
 		      </div>
 		      <div class=\"modal-body\"
-      
-		      <h1>Add Person</h1>
-    <form action = "add_person_success.php" method = "get">
-      <input type = "text" name = "upload" style = "display:none" value = "upload">
-      First Name: <input trim="text" name= "fname"><br/>
-      Middle Name: <input trim="text" name= "mname"><br/>
-      Last Name: <input type="text" name= "lname"><br/>
-      Suffix: <select name="position">
-                <option value="">-Select-</option>
-                <option value="II">II</option>
-                <option value="III">III</option>
-                <option value="IV">IV</option>
-                <option value="Sr">Sr</option>
-                <option value="Jr">Jr</option>
-                <option value="MD">MD</option>
-                <option value="DO">DO</option>
-                <option value="PhD">PhD</option>
-                <option value="JD">JD</option>
-                <option value="Ret">Ret</option>
-                <option value="RN">RN</option>
-              </select><br />
-      Position: <select name="position">
-                 <option value="">-Select-</option>
-                 <option value="Physician">Physician</option>
-                 <option value="Officer">Officer</option>
-                 <option value="Volunteer">Volunteer</option>
-                 <option value="Shadowing">Shadowing</option>
-               </select><br />
-      <input type = "submit" value = "submit">
-    </form>
-
+			    Hi
 		      </div>
 		      <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
