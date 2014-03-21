f97df281159bd9779098c1632058c70f87355c9e
nshaywood <nshaywood@gmail.com>
2014-03-21 15:01:00 -0500

    No more center justify

--- includes/menu.php
+++ includes/menu.php
@@ -2,7 +2,7 @@
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="bootstrap/css/starter-template.css" rel="stylesheet">
+    <link href="bootstrap/css/theme.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
