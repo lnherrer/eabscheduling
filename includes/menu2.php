76c09999cb81d85b16f183b556f3d154ea05df24
lnherrer <lnsherrer@gmail.com>
2014-03-21 12:46:22 -0500

    updates 1.3

---
+++ includes/menu2.php
@@ -0,0 +1,52 @@
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap/css/starter-template2.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">EAB Scheduling</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php#about">About EAB</a></li>
+            <li class="dropdown">
+              <a href="index.php#sign_up_now" class="dropdown-toggle" data-toggle="dropdown">Sign Up Now <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="index.php#new_volunteer">New Volunteer</a></li>
+                <li><a href="returning_volunteer.php">Returning Volunteer</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">More Info</li>
+                <li><a href="index.php#volunteer_roles_&_responsibilities">Volunteer Roles & Responsibilities</a></li>
+ 
+              </ul>
+            </li>
+            <li><a href="index.php#officer">For Officers</a></li>
+            <li><a href="index.php#physician">For Physicians</a></li>
+            <li><a href="index.php#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <!-- This is where page content goes -->
--- returning_volunteer.php
+++ returning_volunteer.php
@@ -2,7 +2,7 @@
 
 	<title>Add Person</title>
 
-<?php require_once("includes/menu.php"); ?>
+<?php require_once("includes/menu2.php"); ?>
 
         <h1>Add Person</h1>
 
