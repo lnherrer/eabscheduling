2925198fc4b4f5a7d5abdd0491c4a93d649b7707
jhsung <jhsung.cal@gmail.com>
2014-03-21 11:47:49 -0500

    add_person dropdown test 1.3

--- add_person.php
+++ add_person.php
@@ -17,21 +17,51 @@
       Race: <input types = "text" name = "race"> <br />
       Programming Language: <input type = "text" name = "proglang"> <br />
       Program: <select name="program">
-                 <optgroup label="Select">
+                   <option value="">-Select-</option>
                    <option value="M-Power Clinic">M-Power Clinic</option>
                    <option value="EAB Clinic">EAB Clinic</option>
                    <option value="EAB Screening">EAB Screening</option>
-                 </optgroup>
-               
+               </select><br />
+      Name: <br />
+      Position: <select name="position">
+                 <option value="">-Select-</option>
+                 <option value="Physician">Physician</option>
+                 <option value="Officer">Officer</option>
+                 <option value="Volunteer">Volunteer</option>
+                 <option value="Shadowing">Shadowing</option>
+               </select><br />
+      Role: <select name="role">
+                 <option value="">-Select-</option>
+                 <option value="Physician">Physician</option>
+                 <option value="Front & Back Officer">Front & Back Officer</option>
+                 <option value="Check In/Check Out">Check In/Check Out</option>
+                 <option value="Clinic Leader">Clinic Leader</option>
+                 <option value="Records Officer">Records Officer</option>
+                 <option value="Dispensary Leader">Dispensary Leader</option>
+                 <option value="Education Officer">Education Officer</option>
+                 <option value="Check In">Check In</option>
+                 <option value="Check Out">Check Out</option>
+                 <option value="H&P">H&P</option>
+                 <option value="Dispensary Assistant">Dispensary Assistant</option>
+                 <option value="Vitals">Vitals</option>
+                 <option value="Education Assistant">Education Assistant</option>
+                 <option value="HIV Screener">HIV Screener</option>
+            </select><br />
+      Class: <select name="class">
+                 <option value="Attending">Attending</option>
+                 <option value="Fellow">Fellow</option>
+                 <option value="Chief Resident">Chief Resident</option>
+                 <option value="MS1">MS2</option>
+                 <option value="MS2">MS2</option>
+                 <option value="MS3">MS3</option>
+                 <option value="MS4">MS4</option>
+                 <option value="Physical Therapy Student">Physical Therapy Student</option>
+                 <option value="Pharmacy Student">Pharmacy Student</option>
+                 <option value="Public Health Student">Public Health Student</option>
+                 <option value="Social Work Volunteer">Social Work Volunteer</option>
+                 <option value="Dental Student">Dental Student</option>
+              </select><br />
 
-
-
-
-
-
-
-
-         </select>
       <input type = "submit" value = "submit">
     </form>
   </body>
