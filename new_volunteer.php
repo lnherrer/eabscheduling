22c4ee56f3129ca52492b2292cb5d4f9976d8be4
Omar Ramadan <ramoram3@gmail.com>
2014-03-21 17:09:13 -0500

    new volunteer changes

--- new_volunteer.php
+++ new_volunteer.php
@@ -1,10 +1,10 @@
 <?php require_once("includes/header.php"); ?>
 
-	<title>Add Person</title>
+	<title>EAB Scheduling</title>
 
 <?php require_once("includes/menu.php"); ?>
 
-        <h1>Add Person</h1>
+        <h1>Sign Up Now</h1>
 
 <html>
 
@@ -14,26 +14,26 @@
       Middle Name: <input trim="text" name= "mname"><br/>
       Last Name: <input type="text" name= "lname"><br/>
       Suffix: <select name="position">
-                <option value="">-Select-</option>
-                <option value="II">II</option>
-                <option value="III">III</option>
-                <option value="IV">IV</option>
-                <option value="Sr">Sr</option>
-                <option value="Jr">Jr</option>
-                <option value="MD">MD</option>
-                <option value="DO">DO</option>
-                <option value="PhD">PhD</option>
-                <option value="JD">JD</option>
-                <option value="Ret">Ret</option>
-                <option value="RN">RN</option>
-              </select><br />
+        <option value="">-Select-</option>
+        <option value="II">II</option>
+        <option value="III">III</option>
+        <option value="IV">IV</option>
+        <option value="Sr">Sr</option>
+        <option value="Jr">Jr</option>
+        <option value="MD">MD</option>
+        <option value="DO">DO</option>
+        <option value="PhD">PhD</option>
+        <option value="JD">JD</option>
+        <option value="Ret">Ret</option>
+        <option value="RN">RN</option>
+      </select><br />
       Position: <select name="position">
-                 <option value="">-Select-</option>
-                 <option value="Physician">Physician</option>
-                 <option value="Officer">Officer</option>
-                 <option value="Volunteer">Volunteer</option>
-                 <option value="Shadowing">Shadowing</option>
-               </select><br />
+        <option value="">-Select-</option>
+        <option value="Physician">Physician</option>
+        <option value="Officer">Officer</option>
+        <option value="Volunteer">Volunteer</option>
+        <option value="Shadowing">Shadower</option>
+      </select><br />
       <input type = "submit" value = "submit">
     </form>
   </body>
