b5f1bb975ee20cb7cc8a3a80db9bc545c89ba0e7
nshaywoo <nshaywood@gmail.com>
2014-03-21 08:26:38 -0500

    Nathan's test commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php require_once("includes/menu.php"); ?>
 
         <h1>EAB Scheduling</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p>This is Nathan's edit!</p>
         <p><a href="#" class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
 
 <?php require_once("includes/footer.php"); ?>
