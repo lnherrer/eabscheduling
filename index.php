1a528b44127d9d1b4a2836373e7bd90e674c9b7e
Omar Ramadan <ramoram3@gmail.com>
2014-03-21 15:54:49 -0500

    index text updates 4

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 
 
         <h1>EAB Scheduling</h1>
-        <p>Last test commit!</p>
+        <p>Welcome to the new and revamped EAB Scheduling homepage!</p><br /><br />
         <p>Haven't volunteered with EAB (at EAB Clinic, M-Power, or a community screening) before? Sign up now!</p>
-        <p><a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a></p><br /><br />
+        <p><a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a></p><br />
         <p>Volunteered with EAB already but just can't get enough? Sign up again!</p>
-        <p><a href="returning_volunteer.php" class="btn btn-primary btn-lg" role="button">Returning Volunteer &raquo;</a></p><br /><br />
-        <p>Prefer calendar mode? You do you!</p>
-        <p><a href="calendar.php" class="btn btn-primary btn-lg" role="button">Calendar/Schedule &raquo;</a></p><br /><br />
+        <p><a href="returning_volunteer.php" class="btn btn-primary btn-lg" role="button">Returning Volunteer &raquo;</a></p><br />
+        <p>Prefer calendar mode? Be our guest!</p>
+        <p><a href="calendar.php" class="btn btn-primary btn-lg" role="button">Calendar/Schedule &raquo;</a></p><br />
 
 <?php require_once("includes/footer.php"); ?>
