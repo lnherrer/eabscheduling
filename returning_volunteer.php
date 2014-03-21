572230e0957959054ac485351f0c607b1cbb54aa
lnherrer <lnsherrer@gmail.com>
2014-03-21 12:24:28 -0500

    updates 1.1

--- returning_volunteer.php
+++ returning_volunteer.php
@@ -10,12 +10,6 @@
 
     <form action = "add_person_success.php" method = "get">
       <input type = "text" name = "upload" style = "display:none" value = "upload">
-      Last Name: <input type = "text" name = "lname"> <br />
-      First Name: <input type = "text" name = "fname"> <br />
-      Sex: <input type="radio" name="sex" value="M"> Male
-           <input type="radio" name="sex" value="F"> Female <br />
-      Race: <input types = "text" name = "race"> <br />
-      Programming Language: <input type = "text" name = "proglang"> <br />
       Program: <select name="program">
                    <option value="">-Select-</option>
                    <option value="M-Power Clinic">M-Power Clinic</option>
