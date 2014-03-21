6a444360f6c50a5dc80ef04306fcfb23d0ccd1d2
lnherrer <lnsherrer@gmail.com>
2014-03-21 15:30:20 -0500

    button 1.1

--- includes/menu.php
+++ includes/menu.php
@@ -60,7 +60,7 @@ if($action == "newvolunteer")
 		    <div class=\"modal-content\">
 		      <div class=\"modal-header\">
 		        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>
-		        <h4 class=\"modal-title\" id=\"New Volunteer\">New Volunteer</h4>
+		        <h4 class=\"modal-title\" id=\"newvolunteer\">New Volunteer</h4>
 		      </div>
 		      <div class=\"modal-body\">
 			    Hi
@@ -69,7 +69,6 @@ if($action == "newvolunteer")
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary">Save changes</button>
               </div>
-
 		    </div>
 		  </div>
 		</div>
