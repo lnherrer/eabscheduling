6321f88bfc7ca3c65e3eb37006157b21155a5326
Omar Ramadan <ramoram3@gmail.com>
2014-03-21 15:34:04 -0500

    index edits

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 
         <h1>EAB Scheduling</h1>
         <p>Last test commit!</p>
-        <p><a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a><a href="returning_volunteer.php" class="btn btn-primary btn-lg" role="button">Returning Volunteer &raquo;</a></p><br />
-        <p><a href="calendar.php" class="btn btn-primary btn-lg" role="button">Calendar/Schedule &raquo;</a></p>
+        <p>Haven't volunteered with EAB before? Register as a volunteer now!<a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a></p><br />
+        <p>Volunteered with EAB already but just can't get enough? Sign up again!<a href="returning_volunteer.php" class="btn btn-primary btn-lg" role="button">Returning Volunteer &raquo;</a></p><br />
+        <p>Prefer calendar mode?<a href="calendar.php" class="btn btn-primary btn-lg" role="button">Calendar/Schedule &raquo;</a></p>
 
 <?php require_once("includes/footer.php"); ?>
