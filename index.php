ca336d6d2861b317fcc1f9d5a7ac177a0e2dcbd8
nshaywood <nshaywood@gmail.com>
2014-03-21 15:30:36 -0500

    Take out "last time"

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 
         <h1>EAB Scheduling</h1>
-        <p>Last test commit!</p>
+        <p></p>
         <p><a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a><a href="returning_volunteer.php" class="btn btn-primary btn-lg" role="button">Returning Volunteer &raquo;</a></p><br />
         <p><a href="calendar.php" class="btn btn-primary btn-lg" role="button">Calendar/Schedule &raquo;</a></p>
 
