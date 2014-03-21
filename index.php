141f8bdc9a32270df7fde9703fd8577e57801587
Omar Ramadan <ramoram3@gmail.com>
2014-03-21 15:42:25 -0500

    index text updates

--- index.php
+++ index.php
@@ -5,15 +5,9 @@
 
 
         <h1>EAB Scheduling</h1>
-<<<<<<< HEAD
-        <p></p>
-        <p><a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a><a href="returning_volunteer.php" class="btn btn-primary btn-lg" role="button">Returning Volunteer &raquo;</a></p><br />
-        <p><a href="calendar.php" class="btn btn-primary btn-lg" role="button">Calendar/Schedule &raquo;</a></p>
-=======
         <p>Last test commit!</p>
-        <p>Haven't volunteered with EAB before? Register as a volunteer now!<a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a></p><br />
+        <p>Haven't volunteered with EAB (at EAB Clinic, M-Power, or a community screening) before? Sign up now!<a href="new_volunteer.php" class="btn btn-primary btn-lg" role="button">New Volunteer &raquo;</a></p><br />
         <p>Volunteered with EAB already but just can't get enough? Sign up again!<a href="returning_volunteer.php" class="btn btn-primary btn-lg" role="button">Returning Volunteer &raquo;</a></p><br />
-        <p>Prefer calendar mode?<a href="calendar.php" class="btn btn-primary btn-lg" role="button">Calendar/Schedule &raquo;</a></p>
->>>>>>> 6321f88bfc7ca3c65e3eb37006157b21155a5326
+        <p>Prefer calendar mode? You do you!<a href="calendar.php" class="btn btn-primary btn-lg" role="button">Calendar/Schedule &raquo;</a></p>
 
 <?php require_once("includes/footer.php"); ?>
